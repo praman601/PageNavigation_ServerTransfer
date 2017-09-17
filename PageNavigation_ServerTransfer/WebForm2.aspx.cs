8c85beb2dccb46aee4e4226701d22c1311d43e28
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 16:30:04 +0545

    another Commit

---
+++ PageNavigation_ServerTransfer/WebForm2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageNavigation_ServerTransfer
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.NameValueCollection previousFormCollections = Request.Form;
+            lblName.Text = previousFormCollections["txtName"];
+            lblEmail.Text = previousFormCollections["txtEmail"];
+        }
+    }
+}
